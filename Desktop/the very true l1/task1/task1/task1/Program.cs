e86fbb8a7da499c19ec98c576bbce6f236652dcb
BagdasaryanInga <ingabagdasaryan@ukr.net>
2022-11-11 22:49:10 +0200

    test scripts

---
+++ Desktop/the very true l1/task1/task1/task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main()
+        {
+            /* case 1 xn = 0.75 && n = 2
+            * case 2 xn = -0.75 && n = 3
+            * case 3 xn = 0 && n = 1
+            */
+            double xn = 0;
+            double n = 0;
+            double x0 = 0;
+            double a = 0;
+            double b = 0;
+            double x = 0;
+            double y = 0;
+            Console.WriteLine("x= " + Math.Round(x, 2));
+            Console.WriteLine("y= " + Math.Round(y, 2));
+
+            /* case 1 x1 = 0 && y1 = 0.03; x2 = 0.75 && y2 = 0.12;
+            * case 2 x1 = -0.75 && y1 = 0.03; x2 = -0.75 && y2 = 0.03; x3 = -0.75 && y3 = 0.03; 
+            * case 3 x1 = 0 && y1 = 0.03; 
+            */
+
+        }
+
+    }
+}
+
